5efbba55068955d28ef53a3b32b0fa213ffa4472
Fraser Wilson <blancolioni@gmail.com>
2015-03-10 23:54:25 +0100

    Evaluate_Define now defined in spec Added function random

--- src/machine/lith-primitives.ads
+++ src/machine/lith-primitives.ads
@@ -1,5 +1,13 @@
+with Lith.Objects;
+
 package Lith.Primitives is
 
    procedure Add_Primitives;
+
+   function Evaluate_Define
+     (Store       : in out Lith.Objects.Object_Store'Class;
+      Arguments   : Lith.Objects.Array_Of_Objects;
+      Environment : Lith.Objects.Object)
+      return Lith.Objects.Object;
 
 end Lith.Primitives;
