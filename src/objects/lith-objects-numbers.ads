c232f2cef56c92f2428bf80ffc3af5dc2d277236
Fraser Wilson <fwilson@anago.nl>
2015-03-13 12:40:47 +0100

    Implemented large integer division

--- src/objects/lith-objects-numbers.ads
+++ src/objects/lith-objects-numbers.ads
@@ -33,10 +33,10 @@ package Lith.Objects.Numbers is
    procedure Divide
      (Store : in out Object_Store'Class);
 
-   procedure Remainder
-     (Store : in out Object_Store'Class);
    --  pop the top two objects off the stack, perform the indicated operation,
    --  and push the result.  Both objects must be numbers (of any kind).
    --  The result is the simplest type which can hold it.
+   --  Divide pushes two results onto the stack: the top is the remainder,
+   --  the second element is the quotient
 
 end Lith.Objects.Numbers;
