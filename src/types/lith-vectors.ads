9fb42b7aad867539746d4e246408d2b91b19e5f0
Fraser Wilson <blancolioni@gmail.com>
2015-03-28 23:29:33 +0100

    Added Vector type

---
+++ src/types/lith-vectors.ads
@@ -0,0 +1,39 @@
+private with Ada.Containers.Vectors;
+
+with Lith.Objects;
+
+package Lith.Vectors is
+
+   type Lith_Vector_Type is
+     new Lith.Objects.External_Object_Interface
+   with private;
+
+   overriding function Print
+     (Item  : Lith_Vector_Type;
+      Store : in out Lith.Objects.Object_Store'Class)
+      return Wide_Wide_String;
+
+   overriding function Equal
+     (X, Y  : Lith_Vector_Type;
+      Store : Lith.Objects.Object_Store'Class)
+      return Boolean;
+
+   overriding procedure Finalize
+     (Item  : in out Lith_Vector_Type;
+      Store : in out Lith.Objects.Object_Store'Class);
+
+   procedure Register;
+
+private
+
+   package Object_Vectors is
+     new Ada.Containers.Vectors
+       (Natural, Lith.Objects.Object, Lith.Objects."=");
+
+   type Lith_Vector_Type is
+     new Lith.Objects.External_Object_Interface with
+      record
+         V : Object_Vectors.Vector;
+      end record;
+
+end Lith.Vectors;
