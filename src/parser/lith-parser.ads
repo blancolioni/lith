56794cdd0b8e41dd04d4f47181c0628cf190e97e
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2015-03-29 18:03:43 +0200

    Parser now uses an object store instead of a machine.

--- src/parser/lith-parser.ads
+++ src/parser/lith-parser.ads
@@ -1,15 +1,14 @@
-with Lith.Machine;
 with Lith.Objects;
 
 package Lith.Parser is
 
    function Parse_Expression
-     (Machine : Lith.Machine.Lith_Machine;
+     (Machine : in out Lith.Objects.Object_Store'Class;
       Expr    : Wide_Wide_String)
       return Lith.Objects.Object;
 
    procedure Parse_File
-     (Machine : Lith.Machine.Lith_Machine;
+     (Machine : in out Lith.Objects.Object_Store'Class;
       Path    : String);
 
 end Lith.Parser;
