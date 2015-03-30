79c1e8743d7f7909c9a3aae0d9b240d0119e025d
Fraser Wilson <fwilson@anago.nl>
2015-03-30 15:49:12 +0200

    Fixed mistake in character literal output

--- src/machine/lith-machine.adb
+++ src/machine/lith-machine.adb
@@ -782,7 +782,7 @@ package body Lith.Machine is
       elsif Is_Symbol (Value) then
          return Lith.Symbols.Get_Name (To_Symbol (Value));
       elsif Is_Character (Value) then
-         return "\#" & To_Character (Value);
+         return "#\" & To_Character (Value);
       elsif Is_Function (Value) then
          return Ada.Characters.Conversions.To_Wide_Wide_String
            (Lith.Objects.Hex_Image (Value));
