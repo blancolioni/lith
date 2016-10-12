4abd21b673089ff9c85c0beea60c36d81ae86542
Fraser Wilson <blancolioni@gmail.com>
2016-10-12 12:38:34 +0200

    Lith.Library, Lith.Init: can now ask for a specific core size, and Lith.Library also allows automatic loading of (scheme base)

--- src/driver/lith-init.ads
+++ src/driver/lith-init.ads
@@ -2,7 +2,8 @@ with Lith.Objects;
 
 private package Lith.Init is
 
-   procedure Start_Lith;
+   procedure Start_Lith
+     (Core_Size : Natural := 64 * 1024);
 
    function Main_Store
      return access Lith.Objects.Object_Store'Class;
