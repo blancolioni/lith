06580c08bb389113047c401023de83c0b6093591
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2016-10-28 16:13:20 +0200

    Lith.Library: renamed Store to Library_Store.  Start_Repl and Load now accept an optional store argument

--- src/lith-library.ads
+++ src/lith-library.ads
@@ -9,15 +9,19 @@ package Lith.Library is
    type Object_Store_Instance is access all Lith.Objects.Object_Store'Class;
 
    function New_Instance
-     (Core_Size          : Natural := 64 * 1024)
+     (Core_Size          : Natural := 64 * 1024;
+      Import_Scheme_Base : Boolean := True)
       return Object_Store_Instance;
 
    procedure Close_Instance (Instance : in out Object_Store_Instance);
 
-   function Store return access Lith.Objects.Object_Store'Class;
+   function Library_Store return access Lith.Objects.Object_Store'Class;
 
-   procedure Start_Repl;
+   procedure Start_Repl
+     (With_Store : access Lith.Objects.Object_Store'Class := null);
 
-   procedure Load (Path : String);
+   procedure Load
+     (Path : String;
+      To_Store : access Lith.Objects.Object_Store'Class := null);
 
 end Lith.Library;
