d949edafbc94d5491e5b2eedeaa480fa2ae2e2e3
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2016-11-01 15:51:00 +0100

    Lith.Objects.Interfaces: can now ask Lith to check argument count and types. Evaluation function now has a Name argument for better error reporting.

--- src/objects/lith-objects-interfaces.adb
+++ src/objects/lith-objects-interfaces.adb
@@ -1,22 +1,27 @@
 with Ada.Containers.Indefinite_Vectors;
 with Ada.Containers.Indefinite_Holders;
+with Ada.Containers.Vectors;
 
 with Lith.Parser;
 with Lith.Objects.Symbols;
 
 package body Lith.Objects.Interfaces is
 
+   package Validator_Vectors is
+     new Ada.Containers.Vectors (Positive, Function_Argument_Type);
+
    type Simple_Function_Evaluator is
      new Root_Function_Interface with
       record
-         Eval : Simple_Evaluator;
+         Eval           : Simple_Evaluator;
+         Arg_Constraint : Validator_Vectors.Vector;
       end record;
 
    overriding function Evaluate
-     (Fn : Simple_Function_Evaluator;
+     (Fn    : Simple_Function_Evaluator;
+      Name  : Symbol_Type;
       Store : in out Object_Store'Class)
-     return Object
-   is (Fn.Eval (Store));
+      return Object;
 
    package Function_Interface_Holder is
      new Ada.Containers.Indefinite_Holders (Root_Function_Interface'Class);
@@ -56,14 +61,28 @@ package body Lith.Objects.Interfaces is
 
    procedure Define_Function
      (Name           : String;
+      Eval           : Simple_Evaluator)
+   is
+   begin
+      New_Def (Name,
+               Simple_Function_Evaluator'
+                 (Root_Function_Interface with
+                    Eval           => Eval,
+                    Arg_Constraint => Validator_Vectors.Empty_Vector));
+   end Define_Function;
+
+   ---------------------
+   -- Define_Function --
+   ---------------------
+
+   procedure Define_Function
+     (Name           : String;
       Argument_Count : Natural;
       Eval           : Simple_Evaluator)
    is
       pragma Unreferenced (Argument_Count);
    begin
-      New_Def (Name,
-               Simple_Function_Evaluator'
-                 (Root_Function_Interface with Eval => Eval));
+      Define_Function (Name, Eval);
    end Define_Function;
 
    ---------------------
@@ -78,7 +97,9 @@ package body Lith.Objects.Interfaces is
    begin
       New_Def (Name,
                Simple_Function_Evaluator'
-                 (Root_Function_Interface with Eval => Eval));
+                 (Root_Function_Interface
+                  with Eval      => Eval,
+                  Arg_Constraint => Validator_Vectors.Empty_Vector));
       Store.Define_Top_Level
         (Defs.Last_Element.Name,
          To_Object (Defs.Last_Element.Fn));
@@ -96,6 +117,24 @@ package body Lith.Objects.Interfaces is
       New_Def (Name, Eval);
    end Define_Function;
 
+   ---------------------
+   -- Define_Function --
+   ---------------------
+
+   procedure Define_Function
+     (Name      : String;
+      Arguments : Argument_Type_Array;
+      Eval      : Simple_Evaluator)
+   is
+      Eval_Rec : Simple_Function_Evaluator;
+   begin
+      Eval_Rec.Eval := Eval;
+      for Arg of Arguments loop
+         Eval_Rec.Arg_Constraint.Append (Arg);
+      end loop;
+      Define_Function (Name, Eval_Rec);
+   end Define_Function;
+
    --------------
    -- Evaluate --
    --------------
@@ -107,10 +146,50 @@ package body Lith.Objects.Interfaces is
       return Object
    is
       pragma Unreferenced (Environment);
-      Def : constant Root_Function_Interface'Class :=
-         Defs.Element (Positive (Fn)).Eval.Element;
+      Def  : constant Function_Record := Defs.Element (Positive (Fn));
+      Eval : constant Root_Function_Interface'Class :=
+              Def.Eval.Element;
    begin
-      return Def.Evaluate (Store);
+      return Eval.Evaluate (Def.Name, Store);
+   end Evaluate;
+
+   overriding function Evaluate
+     (Fn    : Simple_Function_Evaluator;
+      Name  : Symbol_Type;
+      Store : in out Object_Store'Class)
+      return Object
+   is
+   begin
+      if not Fn.Arg_Constraint.Is_Empty then
+         if Store.Argument_Count /= Fn.Arg_Constraint.Last_Index then
+            raise Evaluation_Error with
+            Symbols.Get_Name (Name) & ": require"
+              & Natural'Image (Fn.Arg_Constraint.Last_Index)
+              & " arguments, but found"
+              & Natural'Image (Store.Argument_Count);
+         end if;
+
+         for I in 1 .. Store.Argument_Count loop
+            declare
+               V : constant Function_Argument_Type :=
+                     Fn.Arg_Constraint (I);
+            begin
+               if (V.Simple_Validator /= null
+                   and then not V.Simple_Validator (Store.Argument (I)))
+                 or else (V.Validator /= null
+                          and then not V.Validator (Store,
+                                                    Store.Argument (I)))
+               then
+                  raise Evaluation_Error with
+                  Symbols.Get_Name (Name) & ": argument" & Positive'Image (I)
+                    & ": invalid actual (found "
+                    & Store.Show (Store.Argument (I)) & ")";
+               end if;
+            end;
+         end loop;
+      end if;
+
+      return Fn.Eval (Store);
    end Evaluate;
 
    -------------
