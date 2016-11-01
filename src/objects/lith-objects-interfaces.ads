67c900ce1cf4e36cb529541841847ecb151d6b42
Fraser Wilson <blancolioni@gmail.com>
2016-11-01 15:51:00 +0100

    Lith.Objects.Interfaces: can now ask Lith to check argument count and types. Evaluation function now has a Name argument for better error reporting.

--- src/objects/lith-objects-interfaces.ads
+++ src/objects/lith-objects-interfaces.ads
@@ -6,6 +6,10 @@ package Lith.Objects.Interfaces is
 
    procedure Define_Function
      (Name           : String;
+      Eval           : Simple_Evaluator);
+
+   procedure Define_Function
+     (Name           : String;
       Argument_Count : Natural;
       Eval           : Simple_Evaluator);
 
@@ -14,12 +18,46 @@ package Lith.Objects.Interfaces is
       Name           : String;
       Eval           : Simple_Evaluator);
 
+   type Function_Argument_Type is private;
+
+   function Any_Argument_Type return Function_Argument_Type;
+   function Integer_Argument return Function_Argument_Type;
+   function String_Argument return Function_Argument_Type;
+   function Symbol_Argument return Function_Argument_Type;
+   function List_Argument return Function_Argument_Type;
+
+   type Argument_Validator_Function is access
+     function (Store : in out Object_Store'Class;
+               Value : Object)
+     return Boolean;
+
+   type Simple_Argument_Validator_Function is access
+     function (Value : Object)
+               return Boolean;
+
+   function Custom_Argument
+     (Validator : Argument_Validator_Function)
+      return Function_Argument_Type;
+
+   function Custom_Argument
+     (Validator : Simple_Argument_Validator_Function)
+      return Function_Argument_Type;
+
+   type Argument_Type_Array is
+     array (Positive range <>) of Function_Argument_Type;
+
+   procedure Define_Function
+     (Name      : String;
+      Arguments : Argument_Type_Array;
+      Eval      : Simple_Evaluator);
+
    type Root_Function_Interface is interface;
 
    function Evaluate
-     (Fn : Root_Function_Interface;
+     (Fn    : Root_Function_Interface;
+      Name  : Symbol_Type;
       Store : in out Object_Store'Class)
-     return Object
+      return Object
       is abstract;
 
    procedure Define_Function
@@ -42,5 +80,43 @@ package Lith.Objects.Interfaces is
 
    procedure Bind_Primitives
      (Store : in out Object_Store'Class);
+
+private
+
+   type Function_Argument_Type is
+      record
+         Simple_Validator : Simple_Argument_Validator_Function;
+         Validator        : Argument_Validator_Function;
+      end record;
+
+   function Custom_Argument
+     (Validator : Argument_Validator_Function)
+      return Function_Argument_Type
+   is ((null, Validator));
+
+   function Custom_Argument
+     (Validator : Simple_Argument_Validator_Function)
+      return Function_Argument_Type
+   is ((Validator, null));
+
+   function Is_Any (Store : in out Object_Store'Class;
+                    Value : Object)
+                    return Boolean
+   is (True);
+
+   function Any_Argument_Type return Function_Argument_Type
+   is (Custom_Argument (Is_Any'Access));
+
+   function Integer_Argument return Function_Argument_Type
+   is (Custom_Argument (Is_Integer'Access));
+
+   function String_Argument return Function_Argument_Type
+   is (Custom_Argument (Is_String'Access));
+
+   function Symbol_Argument return Function_Argument_Type
+   is (Custom_Argument (Is_Symbol'Access));
+
+   function List_Argument return Function_Argument_Type
+   is (Custom_Argument (Is_Pair'Access));
 
 end Lith.Objects.Interfaces;
