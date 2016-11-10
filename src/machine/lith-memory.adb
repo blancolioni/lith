0597ac68a2000e7a5a07d2d9a9ec0e060774233d
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2016-11-10 16:27:11 +0100

    Lith.Memory: fixed bug where external object could be released when it was present in a GC-triggering cons operation

--- src/machine/lith-memory.adb
+++ src/machine/lith-memory.adb
@@ -158,13 +158,13 @@ package body Lith.Memory is
 
       Flip (Memory);
 
-      Mark (Memory, Memory.Alloc_Car);
-      Mark (Memory, Memory.Alloc_Cdr);
-      Mark (Memory, Memory.Test);
-
       if Memory.Callback /= null then
          Memory.Callback.Before_GC;
       end if;
+
+      Mark (Memory, Memory.Alloc_Car);
+      Mark (Memory, Memory.Alloc_Cdr);
+      Mark (Memory, Memory.Test);
 
       while Memory.Scan < Memory.Free loop
          declare
